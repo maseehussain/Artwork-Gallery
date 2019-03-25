b4c2e30fb24dafca53614b0e3aa43a586d8c85ec
Masee Hussain <hussainmasee@gmail.com>
2019-03-25 13:51:43 +0000

    started from scratch again innital commit

--- config/environment.js
+++
@@ -1,7 +0,0 @@
-const env = process.env.NODE_ENV || "development";
-const port = process.env.PORT || "https://1v11v5kww7.codesandbox.io/";
-const dbURI =
-  process.env.MONGODB_URI || `mongodb://localhost/Artwork-Gallery${env}`;
-const secret = process.env.AUTH_SECRET || "hYgs^=?>@qrTfxLp";
-
-module.exports = { env, port, dbURI, secret };
