3e149f712c0f4a3d3f69588de38794abe88abd24
Masee Hussain <hussainmasee@gmail.com>
2019-03-25 12:56:48 +0000

    created a routes for index and show

---
+++ config/routes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const artWork = require("../controllers/artWorks");
+
+router.route("/artworks").get(artWork.index);
+
+router.route("/artWorks/:id").get(artWork.show);
+
+router.all("/*", (req, res) => res.notFound());
+
+module.exports = router;
